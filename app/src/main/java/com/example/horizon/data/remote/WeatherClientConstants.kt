228378adcdcd68f7237dfb2951cb59908464e6f8
Kanak0510 <kanak05102002@gmail.com>
2025-03-12 20:41:18 +0530

    Refactor : Add WeatherClientConstants object

---
+++ app/src/main/java/com/example/horizon/data/remote/WeatherClientConstants.kt
@@ -0,0 +1,18 @@
+package com.example.horizon.data.remote
+
+object WeatherClientConstants {
+
+    // The base URL of the OpenWeather API
+    const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
+
+    // The endpoints of the OpenWeatherMap API
+    object EndPoints {
+        const val GET_WEATHER_ENDPOINT = "weather"
+    }
+
+    // Used to configure the units returned by the OpenWeatherMap API
+    object Units {
+        const val CELSIUS = "metric"
+        const val FAHRENHEIT = "imperial"
+    }
+}
