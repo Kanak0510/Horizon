e89977c8752a8b82e99611e1bf3cbea5128c9a97
Kanak0510 <kanak05102002@gmail.com>
2025-03-13 20:41:24 +0530

    Fix : Issues in WeatherDetailsCard caused by update tp WeatherDetails class

--- app/src/main/java/com/example/horizon/ui/weather/components/WeatherDetailsCard.kt
+++ app/src/main/java/com/example/horizon/ui/weather/components/WeatherDetailsCard.kt
@@ -12,50 +12,30 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.example.horizon.R
+import com.example.horizon.domain.models.WeatherDetails
 
 /**
- * A data class that contains the weather details required for [WeatherDetailsCard].
+ * A data class that contains all weather icons used in [WeatherDetailsCard].
  *
- * @param minTemp The minimum temperature.
- * @param maxTemp The maximum temperature.
- * @param humidity The humidity.
- * @param windSpeed The wind speed.
- * @param windDirection The wind direction.
- * @param pressure The pressure.
- * @param icons An instance of [WeatherIcons] that would be used in [WeatherDetailsCard].
+ * @param minTempIcon The minimum temperature icon.
+ * @param maxTempIcon The maximum temperature icon.
+ * @param humidityIcon The humidity icon.
+ * @param windSpeedIcon The wind speed icon.
+ * @param windDirectionIcon The wind direction icon.
+ * @param pressureIcon The pressure icon.
  */
-data class WeatherDetailsCard(
-    val minTemp: String,
-    val maxTemp: String,
-    val humidity: String,
-    val windSpeed: String,
-    val windDirection: String,
-    val pressure: String,
-    val icons: WeatherIcons = WeatherIcons()
-) {
-    /**
-     * A data class that contains all weather icons used by [WeatherDetailsCard].
-     *
-     * @param minTempIcon The minimum temperature icon.
-     * @param maxTempIcon The maximum temperature icon.
-     * @param humidityIcon The humidity icon.
-     * @param windSpeedIcon The wind speed icon.
-     * @param windDirectionIcon The wind direction icon.
-     * @param pressureIcon The pressure icon.
-     */
-    data class WeatherIcons(
-        @DrawableRes val minTempIcon: Int = R.drawable.ic_min_temp,
-        @DrawableRes val maxTempIcon: Int = R.drawable.ic_max_temp,
-        @DrawableRes val humidityIcon: Int = R.drawable.ic_humidity,
-        @DrawableRes val windSpeedIcon: Int = R.drawable.ic_wind_speed,
-        @DrawableRes val windDirectionIcon: Int = R.drawable.ic_wind_direction,
-        @DrawableRes val pressureIcon: Int = R.drawable.ic_wind_pressure
-    )
-}
+data class WeatherIcons(
+    @DrawableRes val minTempIcon: Int = R.drawable.ic_min_temp,
+    @DrawableRes val maxTempIcon: Int = R.drawable.ic_max_temp,
+    @DrawableRes val humidityIcon: Int = R.drawable.ic_humidity,
+    @DrawableRes val windSpeedIcon: Int = R.drawable.ic_wind_speed,
+    @DrawableRes val windDirectionIcon: Int = R.drawable.ic_wind_direction,
+    @DrawableRes val pressureIcon: Int = R.drawable.ic_wind_pressure
+)
 
 /**
  * A Weather details card composable that displays the [weatherDetails] with the appropriate
- * icons, with the provided [shortWeatherDescription].
+ * icons, with the provided [weatherDetailsIcons].
  * @param modifier the modifier to be applied to the composable
  * @param cardColors  [CardColors] that will be used to resolve the colors used for this card in
  * different states. See [CardDefaults.cardColors].
@@ -65,7 +45,8 @@ data class WeatherDetailsCard(
 fun WeatherDetailsCard(
     modifier: Modifier = Modifier,
     cardColors: CardColors = CardDefaults.cardColors(),
-    weatherDetails: WeatherDetailsCard,
+    weatherDetails: WeatherDetails,
+    weatherDetailsIcons: WeatherIcons = WeatherIcons(),
     shortWeatherDescription: String
 ) {
     Card(
@@ -94,18 +75,42 @@ fun WeatherDetailsCard(
         ) {
             CardItem(
                 title = "Low",
-                value = weatherDetails.minTemp,
-                imageVector = ImageVector.vectorResource(weatherDetails.icons.minTempIcon)
+                value = weatherDetails.temperature.minTemperature,
+                imageVector = ImageVector.vectorResource(weatherDetailsIcons.minTempIcon)
+            )
+            CardItem(
+                title = "High",
+                value = weatherDetails.temperature.maxTemperature,
+                imageVector = ImageVector.vectorResource(weatherDetailsIcons.maxTempIcon)
+            )
+            CardItem(
+                title = "Humidity",
+                value = weatherDetails.humidity,
+                imageVector = ImageVector.vectorResource(weatherDetailsIcons.humidityIcon)
+            )
+        }
+        Spacer(modifier = Modifier.size(16.dp))
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp)
+                .padding(top = 8.dp, bottom = 16.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            CardItem(
+                title = "Wind Speed",
+                value = weatherDetails.wind.speed,
+                imageVector = ImageVector.vectorResource(weatherDetailsIcons.windSpeedIcon)
             )
             CardItem(
                 title = "Wind Direction",
-                value = weatherDetails.windDirection,
-                imageVector = ImageVector.vectorResource(weatherDetails.icons.windDirectionIcon)
+                value = weatherDetails.wind.direction,
+                imageVector = ImageVector.vectorResource(weatherDetailsIcons.windDirectionIcon)
             )
             CardItem(
                 title = "Pressure",
-                value = weatherDetails.pressure,
-                imageVector = ImageVector.vectorResource(weatherDetails.icons.pressureIcon)
+                value = weatherDetails.humidity,
+                imageVector = ImageVector.vectorResource(weatherDetailsIcons.pressureIcon)
             )
         }
     }
