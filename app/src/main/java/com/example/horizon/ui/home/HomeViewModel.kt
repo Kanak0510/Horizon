a06174c2e04f1e90e50f8cf3961a019680c644a7
Kanak0510 <kanak05102002@gmail.com>
2025-03-15 02:00:33 +0530

    Refactor: Add HomeViewModel

---
+++ app/src/main/java/com/example/horizon/ui/home/HomeViewModel.kt
@@ -0,0 +1,44 @@
+package com.example.horizon.ui.home
+
+import androidx.lifecycle.ViewModel
+import com.example.horizon.data.repositories.location.LocationServicesRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeViewModel @Inject constructor(
+    private val locationServicesRepository: LocationServicesRepository
+) : ViewModel() {
+
+    private val currentSearchQuery = MutableStateFlow("")
+    private val _uiState = MutableStateFlow(UiState.IDLE)
+    val uiState = _uiState as StateFlow<UiState>
+
+    @OptIn(FlowPreview::class)
+    val currentSuggestions = currentSearchQuery.debounce(250)
+        .map { query ->
+            _uiState.value = UiState.LOADING_SUGGESTIONS
+            locationServicesRepository.fetchSuggestedPlacesForQuery(query)
+                .also { _uiState.value = UiState.IDLE }
+        }
+        .filter { it.isSuccess }
+        .map { it.getOrThrow() }
+
+    /**
+     * Used to set the [searchQuery] for which the suggestions should be generated.
+     */
+    fun setSearchQueryForSuggestionsGeneration(searchQuery: String) {
+        currentSearchQuery.value = searchQuery
+    }
+
+    /**
+     * An enum that contains all possible UI states.
+     */
+    enum class UiState { IDLE, LOADING_SUGGESTIONS }
+}
