b28e52eb13e04f1de5d17e78f0fe35e5065f5f0c
Kanak0510 <kanak05102002@gmail.com>
2025-03-15 12:45:32 +0530

    Refactor: Add HorizonWeatherRepositoryTest

--- app/src/test/java/com/example/horizon/data/repositories/weather/HorizonWeatherRepositoryTest.kt
+++ app/src/test/java/com/example/horizon/data/repositories/weather/HorizonWeatherRepositoryTest.kt
@@ -1,3 +1,36 @@
 package com.example.horizon.data.repositories.weather
 
-internal class HorizonWeatherRepositoryTest
+import com.example.horizon.di.NetworkModule
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class HorizonWeatherRepositoryTest {
+
+    private val weatherRepository =
+        HorizonWeatherRepository(NetworkModule.provideWeatherClient())
+
+    @Test
+    fun `getWeatherForLocation should successfully fetch weather details for a given valid coordinate`() =
+        runTest {
+            val latitude = "37.422131"
+            val longitude = "-122.084801"
+            val result = weatherRepository.getWeatherForLocation(latitude, longitude)
+            assert(result.isSuccess)
+            assert(result.getOrNull() != null)
+        }
+
+    @Test
+    fun `getWeatherForLocation should return an exception for an invalid coordinate`() = runTest {
+        /**
+         * This is an invalid coordinate because the latitude and longitude values are outside the
+         * valid range of -90 to 90 and -180 to 180 degrees, respectively.
+         */
+        val latitude = "1000.0"
+        val longitude = "-2000.0"
+        val result = weatherRepository.getWeatherForLocation(latitude, longitude)
+        assert(result.isFailure)
+        assert(result.exceptionOrNull() != null)
+    }
+}
