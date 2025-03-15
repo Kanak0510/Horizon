43c646e4ed24a6e788bd7899732902ea89fbbbf6
Kanak0510 <kanak05102002@gmail.com>
2025-03-15 12:39:47 +0530

    Refactor: Add DI binds method for WeatherRepository

--- app/src/main/java/com/example/horizon/di/RepositoriesModule.kt
+++ app/src/main/java/com/example/horizon/di/RepositoriesModule.kt
@@ -2,6 +2,8 @@ package com.example.horizon.di
 
 import com.example.horizon.data.repositories.location.HorizonLocationServicesRepository
 import com.example.horizon.data.repositories.location.LocationServicesRepository
+import com.example.horizon.data.repositories.weather.HorizonWeatherRepository
+import com.example.horizon.data.repositories.weather.WeatherRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -15,4 +17,9 @@ abstract class RepositoriesModule {
     abstract fun bindLocationServicesRepository(
         impl: HorizonLocationServicesRepository
     ): LocationServicesRepository
+
+    @Binds
+    abstract fun bindWeatherRepository(
+        impl: HorizonWeatherRepository
+    ): WeatherRepository
 }
