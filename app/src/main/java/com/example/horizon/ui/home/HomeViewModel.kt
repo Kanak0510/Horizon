234d5c14439df1772b91135c2f670e048b5785ce
Kanak0510 <kanak05102002@gmail.com>
2025-03-16 23:59:50 +0530

    Refactor: Add weatherDetailsOfSavedLocations property to HomeViewModel

--- app/src/main/java/com/example/horizon/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/horizon/ui/home/HomeViewModel.kt
@@ -1,19 +1,25 @@
 package com.example.horizon.ui.home
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.horizon.data.repositories.location.LocationServicesRepository
+import com.example.horizon.data.repositories.weather.WeatherRepository
+import com.example.horizon.domain.models.BriefWeatherDetails
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
-    private val locationServicesRepository: LocationServicesRepository
+    private val locationServicesRepository: LocationServicesRepository,
+    private val weatherRepository: WeatherRepository
 ) : ViewModel() {
 
     private val currentSearchQuery = MutableStateFlow("")
@@ -29,6 +35,16 @@ class HomeViewModel @Inject constructor(
         }
         .filter { it.isSuccess }
         .map { it.getOrThrow() }
+
+    private val _weatherDetailsOfSavedLocations = MutableStateFlow<List<BriefWeatherDetails>>(emptyList())
+    val weatherDetailsOfSavedLocations = _weatherDetailsOfSavedLocations as StateFlow<List<BriefWeatherDetails>>
+
+    init {
+        weatherRepository
+            .getWeatherStreamForPreviouslySavedLocations()
+            .onEach { _weatherDetailsOfSavedLocations.value = it }
+            .launchIn(viewModelScope)
+    }
 
     /**
      * Used to set the [searchQuery] for which the suggestions should be generated.
