b1b640c96f9dc8f5cfbc7bdd16bb80a9b6987c06
Kanak0510 <kanak05102002@gmail.com>
2025-03-17 11:44:18 +0530

    Refactor: Add HomeViewModel #deleteSavedWeatherLocation

--- app/src/main/java/com/example/horizon/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/horizon/ui/home/HomeViewModel.kt
@@ -14,6 +14,7 @@ import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -58,6 +59,12 @@ class HomeViewModel @Inject constructor(
      */
     fun setSearchQueryForSuggestionsGeneration(searchQuery: String) {
         currentSearchQuery.value = searchQuery
+    }
+
+    fun deleteSavedWeatherLocation(briefWeatherDetails: BriefWeatherDetails) {
+        viewModelScope.launch {
+            weatherRepository.deleteWeatherLocationFromSavedItems(briefWeatherDetails)
+        }
     }
 
     /**
