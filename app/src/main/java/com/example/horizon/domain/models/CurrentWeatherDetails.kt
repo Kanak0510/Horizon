4c4cb2eb336509202fee38df29da41ba40b70045
Kanak0510 <kanak05102002@gmail.com>
2025-03-20 20:15:48 +0530

    Refactor: Add CurrentWeatherDetails #toBriefWeatherDetails mapper extension

--- app/src/main/java/com/example/horizon/domain/models/CurrentWeatherDetails.kt
+++ app/src/main/java/com/example/horizon/domain/models/CurrentWeatherDetails.kt
@@ -6,11 +6,23 @@ import androidx.annotation.DrawableRes
  * A data class that holds the current weather details for a specific location.
  */
 data class CurrentWeatherDetails(
-    val nameOfLocation:String,
+    val nameOfLocation: String,
     val temperature: String,
     val isDay: Int,
     @DrawableRes val iconResId: Int,
     @DrawableRes val imageResId: Int,
     val latitude: String,
     val longitude: String,
+)
+
+/**
+ * Used to convert an instance of [CurrentWeatherDetails] to an instance of [toBriefWeatherDetails].
+ */
+fun CurrentWeatherDetails.toBriefWeatherDetails(): BriefWeatherDetails = BriefWeatherDetails(
+    nameOfLocation = nameOfLocation,
+    currentTemperature = temperature,
+    shortDescription = "N/A", //todo
+    shortDescriptionIcon = iconResId,
+    latitude = latitude,
+    longitude = longitude
 )
