a6634bd592e4b2f8a75b60e56ecf559e00c0afb2
Kanak0510 <kanak05102002@gmail.com>
2025-03-24 19:32:04 +0530

    Refactor: Define "HourlyForecastItems" & "TimeFormats" objects in WeatherClientConstants

--- app/src/main/java/com/example/horizon/data/remote/weather/WeatherClientConstants.kt
+++ app/src/main/java/com/example/horizon/data/remote/weather/WeatherClientConstants.kt
@@ -14,7 +14,7 @@ object WeatherClientConstants {
      * The endpoints of the OpenWeatherMap API
      */
     object EndPoints {
-        const val GET_WEATHER_ENDPOINT = "forecast"
+        const val GET_WEATHER_ENDPOINT = "forecast" // todo rename
     }
 
     /**
@@ -35,5 +35,23 @@ object WeatherClientConstants {
             const val MILLIMETERS = "mm"
             const val INCHES = "inch"
         }
+    }
+
+    /**
+     * Contains strings that represent different hourly forecast items available in the API.
+     */
+    object HourlyForecastItems {
+        const val PRECIPITATION_PROBABILITIES = "precipitation_probability"
+        const val WEATHER_CODE = "weathercode"
+        const val TEMPERATURE = "temperature_2m"
+        const val DEFAULT_ITEMS = "$WEATHER_CODE,$PRECIPITATION_PROBABILITIES,$TEMPERATURE"
+    }
+
+    /**
+     * Used to configure the time format returned by the weather API.
+     */
+    object TimeFormats {
+        const val UNIX_EPOCH_TIME_IN_SECONDS = "unixtime"
+        const val ISO_8601 = "iso8601"
     }
 }
