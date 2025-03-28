800851dc363e247bc6e757245210c1661b154b2e
Kanak0510 <kanak05102002@gmail.com>
2025-03-28 20:36:43 +0530

    Refactor: Revamp base theme and support dynamic colors

--- app/src/main/java/com/example/horizon/ui/theme/Theme.kt
+++ app/src/main/java/com/example/horizon/ui/theme/Theme.kt
@@ -1,21 +1,48 @@
 package com.example.horizon.ui.theme
 
-import androidx.compose.foundation.isSystemInDarkTheme
+import android.os.Build
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 
-private val DarkColorScheme = darkColorScheme()
-private val LightColorScheme = lightColorScheme()
+private val HorizonDarkColorScheme = darkColorScheme(
+    primary = Blue80,
+    onPrimary = Blue20,
+    primaryContainer = Blue30,
+    onPrimaryContainer = Blue90,
+    inversePrimary = Blue40,
+    secondary = DarkBlue80,
+    onSecondary = DarkBlue20,
+    secondaryContainer = DarkBlue30,
+    onSecondaryContainer = DarkBlue90,
+    tertiary = Yellow80,
+    onTertiary = Yellow20,
+    tertiaryContainer = Yellow30,
+    onTertiaryContainer = Yellow90,
+    error = Red80,
+    onError = Red20,
+    errorContainer = Red30,
+    onErrorContainer = Red90,
+    background = Grey10,
+    onBackground = Grey90,
+    surface = Grey10,
+    onSurface = Grey80,
+    inverseSurface = Grey90,
+    inverseOnSurface = Grey20,
+    surfaceVariant = BlueGrey30,
+    onSurfaceVariant = BlueGrey80,
+    outline = BlueGrey60
+)
 
 @Composable
-fun HorizonTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable () -> Unit
-) {
+fun HorizonTheme(areDynamicColorsEnabled: Boolean = true, content: @Composable () -> Unit) {
+    val doesDeviceSupportDynamicColors = areDynamicColorsEnabled && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
+    val context = LocalContext.current
     MaterialTheme(
-        colorScheme = if (darkTheme) DarkColorScheme else LightColorScheme,
+        colorScheme = if (doesDeviceSupportDynamicColors) dynamicDarkColorScheme(context)
+        else HorizonDarkColorScheme,
         typography = Typography,
         content = content
     )
