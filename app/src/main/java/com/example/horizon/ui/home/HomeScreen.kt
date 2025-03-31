e40aa52be691814d741e1804cc6b8331d4d1cc5a
Kanak0510 <kanak05102002@gmail.com>
2025-03-31 11:27:02 +0530

    Fix: Add navigationBars Padding to LazyCloumn in HomeScreen

--- app/src/main/java/com/example/horizon/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/horizon/ui/home/HomeScreen.kt
@@ -14,9 +14,12 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -90,7 +93,8 @@ fun HomeScreen(
     Box {
         LazyColumn(
             modifier = modifier,
-            verticalArrangement = Arrangement.spacedBy(16.dp)
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            contentPadding = WindowInsets.navigationBars.asPaddingValues()
         ) {
             item {
                 Header(
@@ -163,6 +167,10 @@ fun HomeScreen(
                     onClick = { onSavedLocationItemClick(it) },
                     swipeToDismissBoxState = swipeToDismissBoxState
                 )
+            }
+
+            item {
+
             }
         }
         SnackbarHost(
