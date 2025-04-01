7f1ad42b73a8a5ab62889c4cc3e07e245e097b46
Kanak0510 <kanak05102002@gmail.com>
2025-04-01 22:41:00 +0530

    Refactor: Add DeleteMarkedItemsWorker

---
+++ app/src/main/java/com/example/horizon/data/workers/DeleteMarkedItemsWorker.kt
@@ -0,0 +1,35 @@
+package com.example.horizon.data.workers
+
+import android.app.Application
+import androidx.hilt.work.HiltWorker
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.example.horizon.data.local.weather.HorizonDatabaseDao
+import com.example.horizon.di.IODispatcher
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedInject
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
+
+/**
+ * A worker that deletes all items marked as deleted in the database.
+ */
+@HiltWorker
+class DeleteMarkedItemsWorker @AssistedInject constructor(
+    @Assisted application: Application,
+    @Assisted workerParameters: WorkerParameters,
+    private val dao: HorizonDatabaseDao,
+    @IODispatcher private val ioDispatcher: CoroutineDispatcher
+) : CoroutineWorker(application, workerParameters) {
+
+    override suspend fun doWork(): Result = withContext(ioDispatcher) {
+        try {
+            dao.deleteAllItemsMarkedAsDeleted()
+            Result.success()
+        } catch (exception: Exception) {
+            if (exception is CancellationException) throw exception
+            Result.failure()
+        }
+    }
+}
--- app/src/main/java/com/example/horizon/di/Horizon.kt
+++ app/src/main/java/com/example/horizon/di/Horizon.kt
@@ -1,15 +1,26 @@
 package com.example.horizon.di
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
 import com.example.horizon.BuildConfig
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
+import javax.inject.Inject
 
 @HiltAndroidApp
-class Horizon : Application() {
+class Horizon : Application(), Configuration.Provider {
 
+    @Inject
+    lateinit var hiltWorkerFactory: HiltWorkerFactory
     override fun onCreate() {
         super.onCreate()
         if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
     }
+
+    override val workManagerConfiguration: Configuration
+        get() = Configuration.Builder()
+            .setWorkerFactory(hiltWorkerFactory)
+            .build()
+
 }
