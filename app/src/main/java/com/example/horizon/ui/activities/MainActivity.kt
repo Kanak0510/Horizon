deeeb551c703f669430eaaf10370bd02c258e64e
Kanak0510 <kanak05102002@gmail.com>
2025-04-06 01:25:03 +0530

    Refactor: Call installSplashScreen() in MainActivity & Add High Resolution Splash Image

--- app/src/main/java/com/example/horizon/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/horizon/ui/activities/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.Surface
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
 import com.example.horizon.ui.navigation.HorizonNavigation
 import com.example.horizon.ui.theme.HorizonTheme
@@ -13,6 +14,7 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
         enableEdgeToEdge()
